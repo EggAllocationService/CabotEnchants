c0cb16aef0ab0deb02a094b9baec4bcd2a4872c0
Kyle Smith <kyle@cabotmc.dev>
2024-12-15 20:22:41 -0400

    Fix typo

--- src/main/java/dev/cabotmc/cabotenchants/god/GodRewardStep.java
+++ src/main/java/dev/cabotmc/cabotenchants/god/GodRewardStep.java
@@ -1,6 +1,7 @@
 package dev.cabotmc.cabotenchants.god;
 
 import dev.cabotmc.cabotenchants.quest.QuestStep;
+import dev.cabotmc.cabotenchants.util.Models;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
@@ -70,7 +71,7 @@ public class GodRewardStep extends QuestStep {
                     .color(NamedTextColor.DARK_GRAY)
     );
     m.lore(lore);
-    m.setItemModel(new NamespacedKey());
+    m.setItemModel(Models.COSMIC_BOOK);
     i.setItemMeta(m);
     return i;
   }
