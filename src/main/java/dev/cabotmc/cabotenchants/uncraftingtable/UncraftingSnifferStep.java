7a47093211b1da28c341f3553b177ae29eddc3af
Kyle Smith <kyle@cabotmc.dev>
2026-09-02 18:31:03 -0300

    make sniffers only sniff unbreaking step once

--- src/main/java/dev/cabotmc/cabotenchants/uncraftingtable/UncraftingSnifferStep.java
+++ src/main/java/dev/cabotmc/cabotenchants/uncraftingtable/UncraftingSnifferStep.java
@@ -1,15 +1,18 @@
 package dev.cabotmc.cabotenchants.uncraftingtable;
 
 import dev.cabotmc.cabotenchants.quest.QuestStep;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.Random;
 
 public class UncraftingSnifferStep extends QuestStep {
     private double chance = 0.0;
+    private static final NamespacedKey FOUND_NEXT_STEP = new NamespacedKey("cabot", "found_next_step");
 
     private Random random = new Random();
 
@@ -25,11 +28,12 @@ public class UncraftingSnifferStep extends QuestStep {
 
     @EventHandler
     public void sniff(EntityDropItemEvent e) {
-        if (e.getEntityType() == EntityType.SNIFFER) {
+        if (e.getEntityType() == EntityType.SNIFFER && !e.getEntity().getPersistentDataContainer().has(FOUND_NEXT_STEP)) {
             if (random.nextDouble() < chance) {
                 e.getItemDrop().setItemStack(
                         getNextStep().createStepItem()
                 );
+                e.getEntity().getPersistentDataContainer().set(FOUND_NEXT_STEP, PersistentDataType.BOOLEAN, true);
             }
         }
     }
